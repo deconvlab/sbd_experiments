26a9a3d065c695916c98218edca47cd4d08d54bd
Yenson <y.lau@columbia.edu>
2018-09-01 03:04:12 -0400

    Testing PT DQ

--- phase_transition/sbd_pt_dq.m
+++ phase_transition/sbd_pt_dq.m
@@ -3,7 +3,7 @@ clear; %clc; %#ok<*PFBNS>
 run('../initpkg.m');
 
 %% Settings
-params.theta = 10.^linspace(-2.4, -1.4, 5);
+params.theta = 10.^linspace(-2.5, -1.5, 5);
 params.p = ceil(10.^linspace(3, 4, 5));
 params.trials = 5;
 params.saveconvdata = false;
