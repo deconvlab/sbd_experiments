ed14e46668de09a7d1db3a8634d574643b5ce337
Yenson <y.lau@columbia.edu>
2018-09-04 17:37:49 -0400

    PT DQ run 3

--- phase_transition/pt_dq.m
+++ phase_transition/pt_dq.m
@@ -3,15 +3,16 @@ clear; %clc; %#ok<*PFBNS>
 run('../initpkg.m');
 
 %% Settings
-params.trials = 10;
+params.vars = { ...
+  'log_t' linspace(-2.5, -2.0, 5); ...
+  'log_p'     linspace(3.2, 4.2, 5) ...
+};
+params.trials = 5;
+params.saveconvdata = false;
+
 params.saveconvdata = false;
 params.backup = 'results_backup';
 params.n_workers = Inf;
-
-params.vars = { ...
-  'log_t' linspace(-2.5, -2.0, 10); ...
-  'log_p'     linspace(3.2, 4.2, 10) ...
-};
 
 m = @(log_p) 100*round(10^log_p);
 params.xdist = @(log_t, log_p) (rand(m(log_p),1) <= 10^log_t) .* randn(m(log_p),1);
